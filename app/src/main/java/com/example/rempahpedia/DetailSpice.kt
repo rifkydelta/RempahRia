6c5dc2950d54275ba1bada0b9c214b96f66c13bb
rifky <andrarifky26@gmail.com>
2024-06-10 11:09:06 +0700

    membuat DetailSpice

---
+++ app/src/main/java/com/example/rempahpedia/DetailSpice.kt
@@ -0,0 +1,20 @@
+package com.example.rempahpedia
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DetailSpice : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detail_spice)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
